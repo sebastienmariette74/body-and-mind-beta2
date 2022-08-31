281980c28cd95d1b2ac30be6dbc1feb80c452fb5
sebastienmariette74 <61980470+sebastienmariette74@users.noreply.github.com>
2022-08-31 17:11:59 +0200

    improvement script.js

--- public/assets/js/scripts.js
+++ public/assets/js/scripts.js
@@ -7,68 +7,82 @@
 // Scripts
 //
 
-// let url = window.location.protocol;
-// console.log(url);
+window.addEventListener("DOMContentLoaded", (event) => {
+  // Toggle the side navigation
+  const sidebarToggle = document.body.querySelector("#sidebarToggle");
+  if (sidebarToggle) {
+    // Uncomment Below to persist sidebar toggle between refreshes
+    // if (localStorage.getItem('sb|sidebar-toggle') === 'true') {
+    //     document.body.classList.toggle('sb-sidenav-toggled');
+    // }
+    sidebarToggle.addEventListener("click", (event) => {
+      event.preventDefault();
+      document.body.classList.toggle("sb-sidenav-toggled");
+      localStorage.setItem(
+        "sb|sidebar-toggle",
+        document.body.classList.contains("sb-sidenav-toggled")
+      );
+    });
+  }
+});
+
+let async = (url, element = null) => {
+  axios
+    .get(url)
+    .then((response) => {
+      if (element != null) {
+      $(`.${element}`).html(response.data);
+      }
+    })
+    .catch((error) => {
+      $(`.${element}`).parent().html = `Erreur: ${error.message}`;
+      console.error("Il y a une erreur dans la requête", error);
+    });
+};
 
 $(document).ready(function () {
-  window.addEventListener("DOMContentLoaded", (event) => {
-    // Toggle the side navigation
-    const sidebarToggle = document.body.querySelector("#sidebarToggle");
-    if (sidebarToggle) {
-      // Uncomment Below to persist sidebar toggle between refreshes
-      // if (localStorage.getItem('sb|sidebar-toggle') === 'true') {
-      //     document.body.classList.toggle('sb-sidenav-toggled');
-      // }
-      sidebarToggle.addEventListener("click", (event) => {
-        event.preventDefault();
-        document.body.classList.toggle("sb-sidenav-toggled");
-        localStorage.setItem(
-          "sb|sidebar-toggle",
-          document.body.classList.contains("sb-sidenav-toggled")
-        );
-      });
-    }
-  });
+  
+  window.onload = () => {     
 
-  window.onload = () => {
+    /*_______________ NAVBAR ________________________*/
+
+    let navLinks = $(".nav-link");
+    let current = location.href;
+
+    for (let navLink of navLinks) {
+      if (
+        navLink.href == current ||
+        navLink.href.split("/").slice(0, 4)[3] ==
+          current.split("/").slice(0, 4)[3]
+      ) {
+        navLink.className.replace("active", "");
+        navLink.className += " active";
+        navLink.style.color = "#fff";
+      }
+    }
 
     /* _____________ ENVOI DE REQUETE ____________________________*/
 
-    let async = (url, element = null) => {
-      axios
-        .get(url)
-        .then((response) => {
-          if (element != null) {
-            $(`.${element}`).html(response.data);
-          }
-        })
-        .catch((error) => {
-          $(`.${element}`).parent().html = `Erreur: ${error.message}`;
-          console.error("Il y a une erreur dans la requête", error);
-        });
-    };
+    
 
     /* _____________ ACTIVATION/DESACTIVATION DE MODULE ____________________________*/
 
-    let activateModule = $(".activate-module");
-    // console.log(activateModule);
-
-    $('#modules').on('click', ".activate-module", function (e){
-      let slugModule = $(this).data('slug');
-      let moduleId = $(this).data('idmodule');
+    $("#modules").on("click", ".activate-module", function (e) {
+      let slugModule = $(this).data("slug");
+      let moduleId = $(this).data("idmodule");
       let url = "";
       let href = window.location.pathname.split("/");
-      if($(this).data('role') === 'admin'){       
+      if ($(this).data("role") === "admin") {
         if (href.includes("partenaires")) {
           url = `${window.location.protocol}//${window.location.host}/partenaires/${slugModule}/${moduleId}/active-module`;
-        } else { 
+        } else {
           url = `${window.location.protocol}//${window.location.host}/structures/${slugModule}/${moduleId}/active-module`;
         }
-        async(url)
+        async(url);
       } else {
-        e.preventDefault()
+        e.preventDefault();
       }
-    })
+    });
 
     // ____________________ MODAL SUPPRESSION D'UN PARTENAIRE/STRUCTURE ____________________//
 
@@ -91,26 +105,14 @@ $(document).ready(function () {
 
     // ____________________ MODAL ACTIVATION D'UN PARTENAIRE/STRUCTURE ____________________//
 
-    // Get the modal
-    let modalSwitch = document.getElementById("modal-switch");
-
-    /* Get the button that opens the modal */
-    let switches = $(".modal-switch-user");
-
-    // Get the <span> element that closes the modal
-    let span = document.getElementById("close-switch");
-
-    // When the user clicks on <span> (x), close the modal
-    // span.addEventListener("click", () => {
-    //   modalSwitch.style.display = "none";
-    // });
-
     // When the user clicks anywhere outside of the modal, close it
     window.onclick = function (event) {
       if (event.target == $("#modal-switch")) {
-        modalSwitch.style.display = "none";
+        $("#modal-switch").css("display", "none");
       }
     };
+
+    $(".modal-switch-user").css("cursor", "pointer");
 
     let slug = "";
     let state = "";
@@ -204,8 +206,6 @@ $(document).ready(function () {
 
   // ______________________ FILTRE DES PARTENAIRES/STRUCTURES PAR NOM, OU ACTIVE/DESACTIVE _________________ //
 
-  
-
   function onClickFilter(event) {
     event.preventDefault();
     let filter = $(this).attr("name");
@@ -227,20 +227,6 @@ $(document).ready(function () {
   }
 
   $(document).on("click", ".js-filter", onClickFilter);
-
-  /*_______________ NAVBAR ________________________*/
-
-  let navLinks = $(".nav-link");
-  let current = location.href;
-
-  for (let navLink of navLinks) {
-    if (navLink.href == current || navLink.href.split("/").slice(0,4)[3] == current.split("/").slice(0,4)[3] ) {
-      navLink.className.replace("active", "");
-      navLink.className += " active";
-      navLink.style.color = "#fff";
-    }
-  }
-
 
   /*_______________ PAGINATION ________________________*/
   $("#content").on("click", ".page-link", function (e) {
