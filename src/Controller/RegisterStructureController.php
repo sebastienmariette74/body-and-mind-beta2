9fcbe04068402977e02c827ca301604a5ddbde12
sebastienmariette74 <61980470+sebastienmariette74@users.noreply.github.com>
2022-09-10 11:11:43 +0200

    config heroku

--- src/Controller/RegisterStructureController.php
+++ src/Controller/RegisterStructureController.php
@@ -84,8 +84,8 @@ class RegisterStructureController extends AbstractController
             $token = $jwt->generate($header, $payload, $this->getParameter('app.jwtsecret'));
 
             $mail->send(
-                // 'noreply@bodyandmind.fr',
-                'bodyandmindfrance@gmail.com',
+                'noreply@bodyandmind.fr',
+                // 'sebastien.mariette.74@gmail.com',
                 $structure->getEmail(),
                 'Activation de votre compte sur le site Body & Mind',
                 'register',
@@ -98,8 +98,8 @@ class RegisterStructureController extends AbstractController
             $url = $this->generateUrl('structures_details', ['slug' => $slug], UrlGeneratorInterface::ABSOLUTE_URL);
 
             $mail->send(
-                // 'noreply@bodyandmind.fr',
-                'bodyandmindfrance@gmail.com',
+                'noreply@bodyandmind.fr',
+                // 'sebastien.mariette.74@gmail.com',
                 $partner->getEmail(),
                 'Activation du compte de votre salle de sport',
                 'info_partner',
